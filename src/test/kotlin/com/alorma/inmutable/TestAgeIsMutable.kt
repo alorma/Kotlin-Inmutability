6604539a5921b5a779ba2c3af8edd01ff9253b69
Bernat Borrás <bernatbor15@gmail.com>
2017-12-12 22:32:08 +0100

    Add test that age is modified WRONG

---
+++ src/test/kotlin/com/alorma/inmutable/TestAgeIsMutable.kt
@@ -0,0 +1,28 @@
+package com.alorma.inmutable
+
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class TestAgeIsMutable {
+
+    private lateinit var student: Student
+
+    private lateinit var age: Age
+
+    @Before
+    fun setup() {
+        age = Age(1, 1, 1992)
+        student = Student(1, "Alex", age)
+    }
+
+    @Test
+    fun yearIsNotModified() {
+        assertEquals(1992, student.age.year)
+
+        age.year = 1993
+
+        assertEquals(1992, student.age.year)
+    }
+
+}
